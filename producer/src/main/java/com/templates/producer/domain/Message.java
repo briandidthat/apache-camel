39503b1f77d55ea856da5ee993e1c55c3bc8c80f
brianthf <blora616@gmail.com>
2021-04-05 16:05:11 -0400

    initial configuration

---
+++ producer/src/main/java/com/templates/producer/domain/Message.java
@@ -0,0 +1,49 @@
+package com.templates.producer.domain;
+
+public class Message {
+    private Long id;
+    private String from;
+    private String to;
+    private String text;
+
+    public Message() {}
+
+    public Message(Long id, String from, String to, String text) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
